5bc00f6c143f8c97d7e90a6a7af5d5d34de9acdd
Victor Alves <vsilva.programador@gmail.com>
2018-06-04 15:44:07 -0300

    add custom error html page

--- src/Handlers/ErrorRequestHandler.php
+++ src/Handlers/ErrorRequestHandler.php
@@ -6,12 +6,24 @@ namespace App\Handlers;
 use Middlewares\ErrorHandlerDefault;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Zend\Diactoros\Response\HtmlResponse;
 
 class ErrorRequestHandler extends ErrorHandlerDefault
 {
+    public function __construct() {
+        $root = dirname(__DIR__, 2);
+        $loader = new \Twig_Loader_Filesystem($root . '/templates');
+        $this->twig = new \Twig_Environment($loader);
+    }
+
     public function handle(Request $request): Response
     {
         $response = parent::handle($request);
+
+        if ($response->getHeaderLine('Content-Type') === 'text/html') {
+            $html = $this->twig->render('error.html', [ 'error' => $request->getAttribute('error') ]);
+            $response = new HtmlResponse($html, $response->getStatusCode(), $response->getHeaders());    
+        }
 
         return $response;
     }
