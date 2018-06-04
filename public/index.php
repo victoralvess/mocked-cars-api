ac8aba8aeff43ccce56580de53d8bbeb29a71a3c
Victor Alves <vsilva.programador@gmail.com>
2018-06-03 21:01:53 -0300

    move autoload require statement to src/bootstrap.php

--- public/index.php
+++ public/index.php
@@ -1,8 +1,5 @@
 <?php
 declare(strict_types = 1);
 
-$ROOT = dirname(__DIR__);
-
-require_once $ROOT . '/vendor/autoload.php';
-require_once $ROOT . '/src/bootstrap.php';
+require_once dirname(__DIR__) . '/src/bootstrap.php';
 
