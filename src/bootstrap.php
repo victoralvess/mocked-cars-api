030122bc23580975ca6c7e060d47e5e93a6a7d4a
Victor Alves <vsilva.programador@gmail.com>
2018-06-03 21:24:01 -0300

    add test route

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,5 +3,26 @@ declare(strict_types = 1);
 
 namespace App;
 
+use FastRoute\RouteCollector;
+use Relay\Relay;
+use Middlewares\FastRoute;
+use Middlewares\RequestHandler;
+use Zend\Diactoros\ServerRequestFactory;
+use function FastRoute\simpleDispatcher;
+
 require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$routes = simpleDispatcher(function (RouteCollector $r) {
+    $r->addRoute('GET', '/info', function ($request) {
+        return phpinfo();
+    });
+});
+
+$queue[] = new FastRoute($routes);
+$queue[] = new RequestHandler();
+
+$relay = new Relay($queue);
+$response = $relay->handle(ServerRequestFactory::fromGlobals());
+
+echo $response->getBody();
 
