6617f8bef71f8d0a1400afc3bcc786e10bead3b7
Victor Alves <vsilva.programador@gmail.com>
2018-06-05 12:19:00 -0300

    update src/bootstrap.php

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,28 +3,24 @@ declare(strict_types = 1);
 
 namespace App;
 
-use App\Handlers\ErrorRequestHandler;
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
 use Relay\Relay;
-use Middlewares\FastRoute;
-use Middlewares\ErrorHandler;
-use Middlewares\RequestHandler;
+use Middlewares\{ErrorHandler, FastRoute, RequestHandler};
+use App\Handlers\ErrorRequestHandler;
 use Zend\Diactoros\ServerRequestFactory;
 use Zend\Diactoros\Response\SapiEmitter;
-use Zend\Diactoros\Response;
 use FastRoute\RouteCollector;
 use function FastRoute\simpleDispatcher;
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
-
-$routes = simpleDispatcher(function (RouteCollector $r) {
-    $r->addRoute('GET', '/info', function ($request) {
-        return phpinfo();
-    });
+$dispatcher = simpleDispatcher(function (RouteCollector $r) {
+    $r->addRoute('GET', '/', function ($request) {
+        return 'GET / - OK';
+    });  
 });
 
-
 $middlewares[] = new ErrorHandler(new ErrorRequestHandler());
-$middlewares[] = new FastRoute($routes);
+$middlewares[] = new FastRoute($dispatcher);
 $middlewares[] = new RequestHandler();
 
 $relay = new Relay($middlewares);
@@ -32,4 +28,3 @@ $response = $relay->handle(ServerRequestFactory::fromGlobals());
 
 $emitter = new SapiEmitter();
 $emitter->emit($response);
-
