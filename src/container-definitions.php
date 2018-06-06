505fea969e48093a9d91a2505e37db255135dff6
Victor Alves <vsilva.programador@gmail.com>
2018-06-06 10:02:20 -0300

    update DI container

--- src/container-definitions.php
+++ src/container-definitions.php
@@ -4,9 +4,18 @@ namespace App;
 
 use App\Handlers\ErrorRequestHandler;
 use App\Handlers\CarsRequestHandler;
+use App\Data\RepositoryInterface;
+use App\Data\CarsRepository;
 use function DI\create;
+use function DI\get;
 
 return [
+    RepositoryInterface::class => get(CarsRepository::class),
+    CarsRepository::class =>
+        create(CarsRepository::class)->constructor(/*get(*/[
+            ['id' => 1, 'name' => 'car1'],
+            ['id' => 2, 'name' => 'car2'],
+        ]/*)*/),
     'ErrorHandler' => create(ErrorRequestHandler::class),
-    'CarsHandler' => create(CarsRequestHandler::class)
+    'CarsHandler' => create(CarsRequestHandler::class),    
 ];
