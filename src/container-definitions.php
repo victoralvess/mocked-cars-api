54f7c00b2d9d06f4b0826257c9435cc26bebcc26
Victor Alves <vsilva.programador@gmail.com>
2018-06-07 12:18:15 -0300

    update container defs

--- src/container-definitions.php
+++ src/container-definitions.php
@@ -5,14 +5,21 @@ namespace App;
 use App\Handlers\ErrorRequestHandler;
 use App\Handlers\CarsRequestHandler;
 use App\Data\RepositoryInterface;
+use App\Data\BuilderInterface;
+use App\Data\CarBuilder;
 use App\Data\CarsRepository;
 use function DI\create;
 use function DI\get;
 
 return [
     RepositoryInterface::class => create(CarsRepository::class),
+    BuilderInterface::class => create(CarBuilder::class),
     'ErrorHandler' => create(ErrorRequestHandler::class),
     'CarsHandler' => get(CarsRequestHandler::class),
     CarsRequestHandler::class =>
-        create(CarsRequestHandler::class)->constructor(get(RepositoryInterface::class)),
+        create(CarsRequestHandler::class)
+            ->constructor(
+                get(RepositoryInterface::class),
+                get(BuilderInterface::class)
+            ),
 ];
